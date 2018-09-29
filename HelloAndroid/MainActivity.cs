ff985900187d799721886c5999ee089ed50f35e9
unknown <you@example.comdedorec2@my.lanecc.edu>
2018-09-29 14:06:01 -0700

    Finished parts 2 and 3 of lab

---
+++ HelloAndroid/MainActivity.cs
@@ -0,0 +1,46 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Runtime;
+using Android.Widget;
+
+namespace HelloAndroid
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+            
+            // Creates the user interface in code
+            var layout = new LinearLayout(this);
+            layout.Orientation = Orientation.Vertical;
+            var aButton = new Button(this);
+            var resetButton = new Button(this);
+            var aLabel = new TextView(this);
+            
+
+            aButton.SetText(Resource.String.helloButtonText);
+            aLabel.SetText(Resource.String.helloLabelText);
+            resetButton.SetText(Resource.String.resetButtonText);
+
+            // button events
+            aButton.Click += (sender, e) => {
+                // set label
+                aLabel.Text = "Hello from the button";
+            };
+
+            resetButton.Click += (sender, e) => {
+                // reset label
+                aLabel.Text = GetString(Resource.String.helloLabelText);
+            };
+
+
+            layout.AddView(aButton);
+            layout.AddView(aLabel);
+            layout.AddView(resetButton);
+            SetContentView(layout);
+        }
+    }
+}
---
+++ Helloandroid.Xml/MainActivity.cs
@@ -0,0 +1,32 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Runtime;
+using Android.Widget;
+
+namespace Helloandroid.Xml
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // ui
+            SetContentView(Resource.Layout.activity_main);
+            var aLabel = FindViewById<TextView>(Resource.Id.helloLabel);
+            var aButton = FindViewById<Button>(Resource.Id.aButton);
+            var resetButton = FindViewById<Button>(Resource.Id.resetButton);
+
+            // button events
+            aButton.Click += (sender, e) => {
+                aLabel.Text = "Hello from the button";
+            };
+
+            resetButton.Click += (sender, e) => {
+                aLabel.Text = GetString(Resource.String.helloLabelText);
+            };
+        }
+    }
+}
